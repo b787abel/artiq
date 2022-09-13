dfb011f2a8c1a9fdc28547716c9cfc3630451096
b787abel <abel.beregi01@gmail.com>
2022-09-13 16:26:46 +0100

    Trying raw strings

--- artiq/master/worker_impl.py
+++ artiq/master/worker_impl.py
@@ -287,7 +287,7 @@ def main():
 
     def write_results():
         
-        file = '/home/bentinel/bec2_control/control/daq_path.pyon'
+        file = r'/home/bentinel/bec2_control/control/daq_path.pyon'
         with open(file, 'r') as f: 
             daq_dir = f.readline()
         filename = "{:09}-{}.h5".format(rid, exp.__name__)
