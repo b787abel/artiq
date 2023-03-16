11d7c01a8b60b1ee626590053277f8dd3acb304c
b787abel <abel.beregi01@gmail.com>
2023-03-16 16:28:25 +0000

    added delays

--- artiq/coredevice/urukul_mod.py
+++ artiq/coredevice/urukul_mod.py
@@ -252,11 +252,13 @@ class CPLD_mod:
             self.cfg_write(self.cfg_reg)
         else:
             proto_rev = urukul_sta_proto_rev(self.sta_read())
+            delay(10*ms)
             self.sta_read()
             if proto_rev != STA_PROTO_REV_MATCH:
                 raise ValueError("Urukul proto_rev mismatch")
         delay(100 * us)  # reset, slack
         self.cfg_write(cfg)
+        delay(10*ms)
         self.sta_read()
         if self.sync_div:
             at_mu(now_mu() & ~0xf)  # align to RTIO/2
