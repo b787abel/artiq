f4c2eb79b7e8de1b217b00fd5fb2296bba3b3e32
b787abel <abel.beregi01@gmail.com>
2023-03-16 16:22:40 +0000

    testing sta read

--- artiq/coredevice/urukul_mod.py
+++ artiq/coredevice/urukul_mod.py
@@ -252,10 +252,12 @@ class CPLD_mod:
             self.cfg_write(self.cfg_reg)
         else:
             proto_rev = urukul_sta_proto_rev(self.sta_read())
+            self.sta_read()
             if proto_rev != STA_PROTO_REV_MATCH:
                 raise ValueError("Urukul proto_rev mismatch")
         delay(100 * us)  # reset, slack
         self.cfg_write(cfg)
+        self.sta_read()
         if self.sync_div:
             at_mu(now_mu() & ~0xf)  # align to RTIO/2
             self.set_sync_div(self.sync_div)  # 125 MHz/2 = 1 GHz/16
