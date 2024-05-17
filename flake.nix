d555d1f6328eea0294f950bc49a187c1e8678ef5
b787abel <abel.beregi01@gmail.com>
2024-05-17 17:17:50 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -226,7 +226,7 @@
       vivado = pkgs.buildFHSUserEnv {
         name = "vivado";
         targetPkgs = vivadoDeps;
-        profile = "set -e; source /tools/Xilinx/Vivado/2022.2/settings64.sh";
+        profile = "set -e; source /opt/Xilinx/Vivado/2022.2/settings64.sh";
         runScript = "vivado";
       };
 
